5f16776982d68609fe32b7259a0d85b49309eff3
Patrik <rambo3swe@gmail.com>
2017-07-04 00:14:37 +0200

    trying to get it work

--- src/MatchMaker.ts
+++ src/MatchMaker.ts
@@ -13,18 +13,19 @@ export interface RoomWithScore {
 
 export class MatchMaker {
 
-  private handlers: {[id: string]: any[]} = {};
-  private availableRooms: {[name: string]: Room[]} = {};
-  private roomsById: {[name: number]: Room} = {};
+  private handlers: { [id: string]: any[] } = {};
+  private availableRooms: { [name: string]: Room[] } = {};
+  private roomsById: { [name: number]: Room } = {};
   private roomCount: number = 0;
 
   // room references by client id
-  protected sessions: {[sessionId: string]: Room} = {};
-  protected connectingClientByRoom: {[roomId: string]: {[clientId: string]: any}} = {};
+  protected sessions: { [sessionId: string]: Room } = {};
+  protected connectingClientByRoom: { [roomId: string]: { [clientId: string]: any } } = {};
 
-  public execute (client: Client, message: any) {
+  public execute(client: Client, message: any) {
     if (message[0] == Protocol.JOIN_ROOM) {
-      this.onJoinRoomRequest(message[1], message[2], false, (err: string, room: Room) => {
+      console.log('whats the message', message);
+      this.onJoinRoomRequest(message[1], message[2] || {}, false, (err: string, room: Room) => {
         if (err) {
           let roomId = (room) ? room.roomId : message[1];
           client.send(msgpack.encode([Protocol.JOIN_ERROR, roomId, err]), { binary: true });
@@ -33,11 +34,11 @@ export class MatchMaker {
 
     } else if (message[0] == Protocol.ROOM_DATA) {
       // send message directly to specific room
-      let room = this.getRoomById( message[1] );
+      let room = this.getRoomById(message[1]);
       if (room) { room.onMessage(client, message[2]); }
 
     } else {
-      this.sessions[ client.sessionId ].onMessage(client, message);
+      this.sessions[client.sessionId].onMessage(client, message);
     }
 
   }
@@ -49,7 +50,7 @@ export class MatchMaker {
    * match-making process. The client will request a new WebSocket connection
    * to effectively join into the room created/joined by this method.
    */
-  public onJoinRoomRequest (roomToJoin: string, clientOptions: ClientOptions, allowCreateRoom: boolean, callback: (err: string, room: Room) => any): void {
+  public onJoinRoomRequest(roomToJoin: string, clientOptions: ClientOptions, allowCreateRoom: boolean, callback: (err: string, room: Room) => any): void {
     var room: Room;
     let err: string;
 
@@ -59,19 +60,19 @@ export class MatchMaker {
       room = this.joinById(roomToJoin, clientOptions);
 
     } else {
-      room = this.requestToJoinRoom( roomToJoin, clientOptions ).room
-        || (allowCreateRoom && this.create( roomToJoin, clientOptions ));
+      room = this.requestToJoinRoom(roomToJoin, clientOptions).room
+        || (allowCreateRoom && this.create(roomToJoin, clientOptions));
     }
 
-    if ( room ) {
+    if (room) {
       //
       // Reserve a seat for clientId
       //
-      if (!this.connectingClientByRoom[ room.roomId ]) {
-        this.connectingClientByRoom[ room.roomId ] = {};
+      if (!this.connectingClientByRoom[room.roomId]) {
+        this.connectingClientByRoom[room.roomId] = {};
       }
 
-      this.connectingClientByRoom[ room.roomId ][ clientOptions.clientId ] = clientOptions;
+      this.connectingClientByRoom[room.roomId][clientOptions.clientId] = clientOptions;
 
     } else {
       err = "join_request_fail";
@@ -83,7 +84,7 @@ export class MatchMaker {
   /**
    * Binds target client to the room running in a worker process.
    */
-  public onJoin (roomId: string, client: Client, callback: (err: string, room: Room) => any): void {
+  public onJoin(roomId: string, client: Client, callback: (err: string, room: Room) => any): void {
     let room = this.roomsById[roomId];
     let clientOptions = this.connectingClientByRoom[roomId][client.id];
     let err: string;
@@ -98,7 +99,7 @@ export class MatchMaker {
       (<any>room)._onJoin(client, clientOptions);
       room.once('leave', this.onClientLeaveRoom.bind(this, room));
 
-      this.sessions[ client.sessionId ] = room;
+      this.sessions[client.sessionId] = room;
 
     } catch (e) {
       console.error(room.roomName, "onJoin:", e.stack);
@@ -108,7 +109,7 @@ export class MatchMaker {
     callback(err, room);
   }
 
-  public onLeave (client: Client, room: Room) {
+  public onLeave(client: Client, room: Room) {
     (<any>room)._onLeave(client, true);
   }
 
@@ -117,49 +118,49 @@ export class MatchMaker {
       return true;
     }
 
-    delete this.sessions[ client.sessionId ];
+    delete this.sessions[client.sessionId];
   }
 
-  public addHandler (name: string, handler: Function, options: any = {}): void {
-    this.handlers[ name ] = [handler, options];
-    this.availableRooms[ name ] = [];
+  public addHandler(name: string, handler: Function, options: any = {}): void {
+    this.handlers[name] = [handler, options];
+    this.availableRooms[name] = [];
   }
 
-  protected hasHandler (name: string) {
-    return this.handlers[ name ] !== undefined;
+  protected hasHandler(name: string) {
+    return this.handlers[name] !== undefined;
   }
 
-  public hasAvailableRoom (roomName: string): boolean {
-    return (this.availableRooms[ roomName ] &&
-      this.availableRooms[ roomName ].length > 0)
+  public hasAvailableRoom(roomName: string): boolean {
+    return (this.availableRooms[roomName] &&
+      this.availableRooms[roomName].length > 0)
   }
 
-  public getRoomById (roomId: number): Room {
-    return this.roomsById[ roomId ];
+  public getRoomById(roomId: number): Room {
+    return this.roomsById[roomId];
   }
 
-  public joinById (roomId: string, clientOptions: ClientOptions): Room {
-    let room = this.roomsById[ roomId ];
+  public joinById(roomId: string, clientOptions: ClientOptions): Room {
+    let room = this.roomsById[roomId];
 
     if (!room) {
-      console.error(`Error: trying to join non-existant room "${ roomId }"`);
+      console.error(`Error: trying to join non-existant room "${roomId}"`);
 
     } else if (!room.requestJoin(clientOptions)) {
-      console.error(`Error can't join "${ clientOptions.roomName }" with options: ${ JSON.stringify(clientOptions) }`);
+      console.error(`Error can't join "${clientOptions.roomName}" with options: ${JSON.stringify(clientOptions)}`);
       room = undefined;
     }
 
     return room;
   }
 
-  public requestToJoinRoom (roomName: string, clientOptions: ClientOptions): RoomWithScore {
+  public requestToJoinRoom(roomName: string, clientOptions: ClientOptions): RoomWithScore {
     let room: Room;
     let bestScore = 0;
 
-    if ( this.hasAvailableRoom( roomName ) ) {
-      for ( var i=0; i < this.availableRooms[ roomName ].length; i++ ) {
-        let availableRoom = this.availableRooms[ roomName ][ i ];
-        let numConnectedClients = availableRoom.clients.length + this.connectingClientByRoom[ availableRoom.roomId ].length;
+    if (this.hasAvailableRoom(roomName)) {
+      for (var i = 0; i < this.availableRooms[roomName].length; i++) {
+        let availableRoom = this.availableRooms[roomName][i];
+        let numConnectedClients = availableRoom.clients.length + this.connectingClientByRoom[availableRoom.roomId].length;
 
         // Check maxClients before requesting to join.
         if (numConnectedClients >= availableRoom.maxClients) {
@@ -180,10 +181,10 @@ export class MatchMaker {
     };
   }
 
-  public create (roomName: string, clientOptions: ClientOptions): Room {
+  public create(roomName: string, clientOptions: ClientOptions): Room {
     let room = null
-      , handler = this.handlers[ roomName ][0]
-      , options = this.handlers[ roomName ][1];
+      , handler = this.handlers[roomName][0]
+      , options = this.handlers[roomName][1];
 
     room = new handler();
 
@@ -199,12 +200,12 @@ export class MatchMaker {
     memshared.set(room.roomId, process.pid);
 
     // imediatelly ask client to join the room
-    if ( room.requestJoin(clientOptions) ) {
+    if (room.requestJoin(clientOptions)) {
       room.on('lock', this.lockRoom.bind(this, roomName, room));
       room.on('unlock', this.unlockRoom.bind(this, roomName, room));
       room.once('dispose', this.disposeRoom.bind(this, roomName, room));
 
-      this.roomsById[ room.roomId ] = room;
+      this.roomsById[room.roomId] = room;
 
       // room always start unlocked
       this.unlockRoom(roomName, room);
@@ -216,7 +217,7 @@ export class MatchMaker {
     return room;
   }
 
-  private lockRoom (roomName: string, room: Room): void {
+  private lockRoom(roomName: string, room: Room): void {
     if (this.hasAvailableRoom(roomName)) {
       let roomIndex = this.availableRooms[roomName].indexOf(room);
       if (roomIndex !== -1) {
@@ -235,9 +236,9 @@ export class MatchMaker {
     }
   }
 
-  private unlockRoom (roomName: string, room: Room) {
-    if (this.availableRooms[ roomName ].indexOf(room) === -1) {
-      this.availableRooms[ roomName ].push(room)
+  private unlockRoom(roomName: string, room: Room) {
+    if (this.availableRooms[roomName].indexOf(room) === -1) {
+      this.availableRooms[roomName].push(room)
 
       // flag current worker has this room id
       memshared.sadd(room.roomName, process.pid);
@@ -248,7 +249,7 @@ export class MatchMaker {
   }
 
   private disposeRoom(roomName: string, room: Room): void {
-    delete this.roomsById[ room.roomId ]
+    delete this.roomsById[room.roomId]
 
     // remove from cache
     memshared.del(room.roomId);
